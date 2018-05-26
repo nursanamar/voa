7bef6f552a94741b8b49618688c39896b5d60f3d
Nursan amar <nursan011@gmail.com>
2018-05-26 15:03:41 +0800

    footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,35 +1,32 @@
 <footer>
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 text-center-mobile">
-					<h3 class="white">Reserve a Free Trial Class!</h3>
-					<h5 class="light regular light-white">Shape your body and improve your health.</h5>
-					<a href="#" class="btn btn-blue ripple trial-button">Start Free Trial</a>
+			<div class="row footer-link " >
+				<div class="col-sm-6 col-md-6" >
+					<div class="col-sm-4 col-md-4" >
+						<a href="">Tentang Kami</a>
+					</div>
+					<div class="col-sm-4 col-md-4" >
+						<a href="">Redaksi</a>
+					</div>
+					<div class="col-sm-4 col-md-4" >
+						<a href="">Pedoman Siber</a>
+					</div>
 				</div>
-				<div class="col-sm-6 text-center-mobile">
-					<h3 class="white">Opening Hours <span class="open-blink"></span></h3>
-					<div class="row opening-hours">
-						<div class="col-sm-6 text-center-mobile">
-							<h5 class="light-white light">Mon - Fri</h5>
-							<h3 class="regular white">9:00 - 22:00</h3>
-						</div>
-						<div class="col-sm-6 text-center-mobile">
-							<h5 class="light-white light">Sat - Sun</h5>
-							<h3 class="regular white">10:00 - 18:00</h3>
-						</div>
+				<div class="col-sm-6 col-md-6" >
+					<div class="col-sm-4 col-md-4" >
+						<a href="">Kontak Kami</a>
+					</div>
+					<div class="col-sm-4 col-md-4" >
+						<a href="">Metodologi Riset</a>
+					</div>
+					<div class="col-sm-4 col-md-4" >
+						<a href="">FAQ</a>
 					</div>
 				</div>
 			</div>
-			<div class="row bottom-footer text-center-mobile">
-				<div class="col-sm-8">
-					<p>&copy; 2015 All Rights Reserved. Powered by <a href="http://www.phir.co/">PHIr</a> exclusively for <a href="http://tympanus.net/codrops/">Codrops</a></p>
-				</div>
-				<div class="col-sm-4 text-right text-center-mobile">
-					<ul class="social-footer">
-						<li><a href="http://www.facebook.com/pages/Codrops/159107397912"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="http://www.twitter.com/codrops"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="https://plus.google.com/101095823814290637419"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
+			<div class="row footer-copyright" >
+				<div class="col-sm-4 col-md-4 col-md-offset-4 col-sm-offset-4 ">
+					<span>© 2018 Voice Alauddin. All rights reserved</span>
 				</div>
 			</div>
 		</div>

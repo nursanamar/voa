0e6b3af0c6145a42db8e20d53420d67bf0d9b06a
Nursan amar <nursan011@gmail.com>
2018-05-29 22:58:39 +0800

    dynamic width and height

--- application/views/video.php
+++ application/views/video.php
@@ -24,8 +24,8 @@
 	<section id="services" class="section player-section">
 		<div class="container">
             <div class="row">
-                <div class="col-sm-9 col-md-9" >
-                    <iframe width="847" height="476" src="https://www.youtube.com/embed/<?php echo $id ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <div id="videoWraper" class="col-sm-9 col-md-9" >
+                    <iframe id="video" src="https://www.youtube.com/embed/<?php echo $id ?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
                 <div class="col-sm-3 col-md-3" >
                     <div class="fb-page" data-href="https://www.facebook.com/facebook" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
@@ -40,6 +40,12 @@
     $(document).ready(function(){
        $(".player-section").css("padding-top",($("nav").height() + 85)+"px");
        console.log($("nav").height());
+       $("#video").height(($("#videoWraper").width()) * 0.5619);
+       $("#video").width(($("#videoWraper").width()));
+    })
+    $(window).on("resize",function() {
+        $("#video").height(($("#videoWraper").width()) * 0.5619);
+       $("#video").width(($("#videoWraper").width()));
     })
 </script> 
 </body>
